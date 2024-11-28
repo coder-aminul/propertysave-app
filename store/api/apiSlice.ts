cb71949b8e5f6cb421d6e4544fc346e7c160d91d
Aminul Islam <aminulislammd96@gmail.com>
2024-11-29 02:47:04 +0600

    env-update

--- store/api/apiSlice.ts
+++ store/api/apiSlice.ts
@@ -5,7 +5,7 @@ export const apiSlice = createApi({
   reducerPath: 'propertysave-app',
   baseQuery: fetchBaseQuery({
     baseUrl:
-      Platform.OS === 'android' ? 'http://10.0.2.2:9002/v1' : process.env.EXPO_PUBLIC_API_URL, // Replace with your API's base URL
+      Platform.OS === 'android' ? process.env.EXPO_PUBLIC_API_URL : process.env.EXPO_PUBLIC_API_URL, // Replace with your API's base URL
   }),
   endpoints: (builder) => ({}),
   tagTypes: [],
